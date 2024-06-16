cee4a008adceb2b409e1c225d4932c7239b537e0
Kamil Uyanik <uyanik@uni-bremen.de>
2024-06-16 03:53:22 +0200

    Implement: ProgressView for Fetching User

--- DIY-Network/DIY-Network/Core/Profile/View/EditProfileView.swift
+++ DIY-Network/DIY-Network/Core/Profile/View/EditProfileView.swift
@@ -12,70 +12,77 @@ struct EditProfileView: View {
   @Environment(\.dismiss) var dismiss
   @EnvironmentObject var authViewModel: AuthViewModel
   @StateObject var viewModel: EditProfileViewModel
+  @State private var showProgressView = false
 
   init(user: UserModel) {
     self._viewModel = StateObject(wrappedValue: EditProfileViewModel(user: user))
   }
 
   var body: some View {
-    VStack {
+    if showProgressView {
+      ProgressView("Updating...")
+    } else {
       VStack {
-        HStack {
-          Button("Cancel") {
-            dismiss()
-          }
-
-          Spacer()
-
-          Text("Edit Your Profile")
-            .font(.subheadline)
-            .fontWeight(.semibold)
-
-          Spacer()
-
-          Button("Done") {
-            Task {
-              try await viewModel.updateUserData()
-              await authViewModel.fetchUser()
+        VStack {
+          HStack {
+            Button("Cancel") {
               dismiss()
             }
+
+            Spacer()
+
+            Text("Edit Your Profile")
+              .font(.subheadline)
+              .fontWeight(.semibold)
+
+            Spacer()
+
+            Button("Done") {
+              Task {
+                showProgressView = true
+                try await viewModel.updateUserData()
+                await authViewModel.fetchUser()
+                showProgressView = false
+                dismiss()
+              }
+            }
+            .font(.subheadline)
+            .fontWeight(.bold)
           }
-          .font(.subheadline)
-          .fontWeight(.bold)
+          .padding(.horizontal)
         }
-        .padding(.horizontal)
-      }
 
-      Divider()
+        Divider()
 
-      PhotosPicker(selection: $viewModel.selectedImage) {
+        PhotosPicker(selection: $viewModel.selectedImage) {
+          VStack {
+            if let image = viewModel.profileImage {
+              image
+                .resizable()
+                .clipShape(Circle())
+                .frame(width: 80, height: 80)
+            } else {
+              CircularProfileImageView(size: 80, imageUrl: viewModel.user.profileImageUrl ?? "")
+            }
+
+            Text("Edit profile picture")
+              .font(.footnote)
+              .fontWeight(.semibold)
+          }
+        }
+        .padding(.vertical, 10)
+
         VStack {
-          if let image = viewModel.profileImage {
-            image
-              .resizable()
-              .clipShape(Circle())
-              .frame(width: 80, height: 80)
-          } else {
-            CircularProfileImageView(size: 80, imageUrl: viewModel.user.profileImageUrl ?? "")
-          }
+          EditProfileRowView(
+            title: "Fullname", placeholder: "Enter your name", text: $viewModel.fullname)
 
-          Text("Edit profile picture")
-            .font(.footnote)
-            .fontWeight(.semibold)
+          EditProfileRowView(title: "Bio", placeholder: "Enter your Bio", text: $viewModel.bio)
         }
-      }
-      .padding(.vertical, 10)
+        .padding()
 
-      VStack {
-        EditProfileRowView(
-          title: "Fullname", placeholder: "Enter your name", text: $viewModel.fullname)
-
-        EditProfileRowView(title: "Bio", placeholder: "Enter your Bio", text: $viewModel.bio)
+        Spacer()
       }
-      .padding()
     }
-
-    Spacer()
   }
 }
 
