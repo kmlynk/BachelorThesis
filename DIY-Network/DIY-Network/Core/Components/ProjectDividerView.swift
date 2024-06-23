ee3dbe80dbaed01644cf97e1fa2dd71a9b8cfeb6
Kamil Uyanik <uyanik@uni-bremen.de>
2024-06-23 17:22:01 +0200

    Update: Generic Width

--- DIY-Network/DIY-Network/Core/Components/ProjectDividerView.swift
+++ DIY-Network/DIY-Network/Core/Components/ProjectDividerView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ProjectDividerView: View {
+  let minusWidth: CGFloat
   let height: CGFloat
 
   var body: some View {
     Rectangle()
-      .frame(width: UIScreen.main.bounds.width, height: height)
+      .frame(
+        width: UIScreen.main.bounds.width - minusWidth,
+        height: height)
   }
 }
 
 #Preview{
-  ProjectDividerView(height: 2)
+  ProjectDividerView(
+    minusWidth: 10,
+    height: 2)
 }
