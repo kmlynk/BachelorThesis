dcc6c3c5d045405881e2ec368ae9a39cb7246449
Kamil Uyanik <uyanik@uni-bremen.de>
2024-06-25 16:54:44 +0200

    Update: Moving ProjectEdit Function to LibraryView

--- DIY-Network/DIY-Network/Core/Library/View/LibraryCell.swift
+++ DIY-Network/DIY-Network/Core/Library/View/LibraryCell.swift
@@ -10,20 +10,37 @@ import SwiftUI
 struct LibraryCell: View {
   @Environment(\.colorScheme) var currentMode
   let project: ProjectModel
+  @State private var showSheet = false
 
   var body: some View {
     HStack {
-      ProjectImageView(
-        width: 100,
-        height: 100,
-        imageUrl: project.projectImageUrl ?? ""
-      )
+      VStack {
+        HStack {
+          Spacer()
 
-      Text(project.projectName)
-        .multilineTextAlignment(.leading)
-        .fontWeight(.heavy)
+          Button {
+            print("DEBUG: Show bottom sheet")
+            showSheet.toggle()
+          } label: {
+            Image(systemName: "ellipsis")
+          }
+          .padding(.trailing, 5)
+        }
 
-      Spacer()
+        HStack {
+          ProjectImageView(
+            width: 100,
+            height: 100,
+            imageUrl: project.projectImageUrl ?? ""
+          )
+
+          Text(project.projectName)
+            .multilineTextAlignment(.leading)
+            .fontWeight(.heavy)
+
+          Spacer()
+        }
+      }
     }
     .padding()
     .background(currentMode == .dark ? Color.black : Color.white)
@@ -32,6 +49,51 @@ struct LibraryCell: View {
     .shadow(color: Color.primary.opacity(0.08), radius: 5, x: -5, y: -5)
     .padding(.horizontal)
     .padding(.top)
+    .sheet(isPresented: $showSheet) {
+      ProjectBottomSheet(project: project)
+        .presentationDetents([.height(150)])
+    }
+  }
+}
+
+struct ProjectBottomSheet: View {
+  @Environment(\.dismiss) var dismiss
+  var project: ProjectModel
+  @State private var showEditView = false
+  @State private var showProgressView = false
+
+  var body: some View {
+    if !showProgressView {
+      List {
+        Button {
+          showEditView.toggle()
+        } label: {
+          HStack(spacing: 10) {
+            Image(systemName: "pencil")
+            Text("Edit Project")
+          }
+        }
+
+        Button {
+          Task {
+            showProgressView.toggle()
+            await LibraryService.deleteProjectData(project: project)
+            dismiss()
+          }
+        } label: {
+          HStack(spacing: 10) {
+            Image(systemName: "trash.fill")
+            Text("Delete Project")
+          }
+          .foregroundColor(Color.red)
+        }
+      }
+      .fullScreenCover(isPresented: $showEditView) {
+        EditProjectView(project: project)
+      }
+    } else {
+      ProgressView("Deleting Project...")
+    }
   }
 }
 
