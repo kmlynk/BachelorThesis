0003e016f64d20d22ee2bcc08c2cde10dbbf5546
Kamil Uyanik <uyanik@uni-bremen.de>
2024-06-25 17:42:45 +0200

    Implement: Mock Data

--- DIY-Network/DIY-Network/DataModels/ProjectStepModel.swift
+++ DIY-Network/DIY-Network/DataModels/ProjectStepModel.swift
@@ -15,3 +15,17 @@ struct ProjectStepModel: Identifiable, Hashable, Codable {
   var stepDesc: String
   var stepImageUrl: String?
 }
+
+extension ProjectStepModel {
+  static var MOCK_STEPS: [ProjectStepModel] = [
+    .init(
+      id: "551A6B66-5E0C-4C96-AA4E-A154C2236781",
+      projectId: "A753F134-DA85-4BA0-B3BD-7792919BEB6C",
+      stepNumber: 1,
+      stepName: "Main Branch Step One",
+      stepDesc: "Test Step One",
+      stepImageUrl:
+        "https://firebasestorage.googleapis.com:443/v0/b/diy-network-75d15.appspot.com/o/images%2FEB06F101-853C-47BB-9566-AFBF934808C5?alt=media&token=81076c42-9ba1-4590-b7d3-fdd1447eea8b"
+    )
+  ]
+}
