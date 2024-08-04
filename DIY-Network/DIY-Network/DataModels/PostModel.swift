20c328b5ac31fa84539b9e15c5c8a40804b6446e
Kamil Uyanik <uyanik@uni-bremen.de>
2024-08-04 18:36:19 +0200

    Update: Post Mocks

--- DIY-Network/DIY-Network/DataModels/PostModel.swift
+++ DIY-Network/DIY-Network/DataModels/PostModel.swift
@@ -23,11 +23,12 @@ struct PostModel: Identifiable, Hashable, Codable {
 extension PostModel {
   static var MOCK_POSTS: [PostModel] = [
     .init(
-      id: NSUUID().uuidString,
-      ownerId: "h9FV9ysmbOff2ZefQJEhjVs3yXi1",
-      projectId: "C2AEDABC-3A3A-4BDF-A2ED-AB2C8F54A4C3",
-      imageUrl: "panda",
-      caption: "This is a test caption for a test project",
+      id: "CF337909-EAFA-482B-B027-5A38A7EB99C7",
+      ownerId: "pwxKOtpz2iau4mI9Eh801GymAPw2",
+      projectId: "3276C1D2-7C16-46F4-9241-B7E2FF518D7C",
+      imageUrl:
+        "https://firebasestorage.googleapis.com:443/v0/b/diy-network-75d15.appspot.com/o/images%2FCA42FCCB-5BF0-4112-A49D-9F77F1ED39CA?alt=media&token=d2e295be-5f86-4b2e-b292-50597fab0733",
+      caption: "This is the first post 🙂",
       likes: 1938,
       timestamp: Timestamp(),
       user: UserModel.MOCK_USERS[0]
