e3ad997bd82519aa18f976191dde2f2ec5ad9dfe
Kamil Uyanik <uyanik@uni-bremen.de>
2024-08-24 02:14:57 +0200

    Implement: LoginView Validation

--- DIY-Network/DIY-Network/Core/Authentication/View/LoginView.swift
+++ DIY-Network/DIY-Network/Core/Authentication/View/LoginView.swift
@@ -11,6 +11,7 @@ struct LoginView: View {
   @EnvironmentObject var authViewModel: AuthViewModel
   @State private var email = ""
   @State private var password = ""
+  @State private var error = ""
 
   var body: some View {
     NavigationStack {
@@ -38,9 +39,28 @@ struct LoginView: View {
         }
         .padding(.top, 15)
 
+        if !error.isEmpty {
+          VStack {
+            Text(error)
+              .animation(.bouncy)
+              .foregroundColor(.red)
+              .font(.footnote)
+              .multilineTextAlignment(.center)
+              .padding(.horizontal)
+          }
+        }
+
         Button {
           Task {
-            try await authViewModel.signIn(withEmail: email, password: password)
+            error = validateLoginForm()
+            if error.isEmpty {
+              do {
+                try await authViewModel.signIn(withEmail: email, password: password)
+              } catch {
+                self.error = authViewModel.authError ?? "Unknown error"
+                print("DEBUG: \(error)")
+              }
+            }
           }
         } label: {
           Text("Login")
@@ -84,6 +104,16 @@ struct LoginView: View {
         }
       }
     }
+  }
+
+  private func validateLoginForm() -> String {
+    if email.isEmpty || password.isEmpty {
+      return "All fields are required."
+    }
+    if !AuthValidator.isValidEmail(email) {
+      return "Invalid email format."
+    }
+    return ""
   }
 }
 
